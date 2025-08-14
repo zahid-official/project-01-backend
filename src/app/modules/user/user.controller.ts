b716ce561c7ca5691991ef8700277f9472bd38af
zahid-official <zahid.official8@gmail.com>
2025-08-14 09:49:43 +0600

    Rename http status

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 import User from "./user.model";
-import statusCodes from "http-status-codes";
+import httpStatus from "http-status-codes";
 
 // Create new user
 const createUser = async (req: Request, res: Response) => {
@@ -12,12 +12,12 @@ const createUser = async (req: Request, res: Response) => {
       email,
     });
 
-    res.status(statusCodes.CREATED).json({
+    res.status(httpStatus.CREATED).json({
       message: "User created successfully",
       data: result,
     });
   } catch (error: any) {
-    res.status(statusCodes.BAD_REQUEST).json({
+    res.status(httpStatus.BAD_REQUEST).json({
       message: error.message ?? "Something went wrong!!",
       error: error,
     });
