525f3ae994f1fe4d923f997eb1362f69921454b5
zahid-official <zahid.official8@gmail.com>
2025-08-19 20:22:19 +0600

    Retrieve all users data using the user retrieval service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -7,9 +7,21 @@ import userService from "./user.service";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await userService.registerUser(req?.body);
-
     res.status(httpStatus.CREATED).json({
       message: "User created successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+// Get all users
+const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await userService.retrieveAllUsers();
+    res.json({
+      message: "Users retrieved successfully",
       data: result,
     });
   } catch (error: any) {
@@ -20,6 +32,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 // User controller object
 const userController = {
   createUser,
+  getAllUsers,
 };
 
 export default userController;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,7 @@
 import { IUser } from "./user.interface";
 import User from "./user.model";
 
+// Register new user
 const registerUser = async (payload: Partial<IUser>) => {
   const { name, email } = payload;
   const user = await User.create({
@@ -11,9 +12,16 @@ const registerUser = async (payload: Partial<IUser>) => {
   return user;
 };
 
+// Retrieve all users
+const retrieveAllUsers = async () => {
+  const users = await User.find();
+  return users;
+};
+
 // User service object
 const userService = {
   registerUser,
+  retrieveAllUsers,
 };
 
 export default userService;
