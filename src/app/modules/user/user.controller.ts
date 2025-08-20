222863236ccd6da8d0623d5e9f1d6a702b4a8416
zahid-official <zahid.official8@gmail.com>
2025-08-20 19:58:03 +0600

    Make utils for catch async function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
