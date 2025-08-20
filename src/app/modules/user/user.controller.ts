728613a176d65f160e5ff945ff1df63623c2329a
zahid-official <zahid.official8@gmail.com>
2025-08-20 19:56:03 +0600

    Make utils for catch async function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,33 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import userService from "./user.service";
+import catchAsync from "../../utils/catchAsync";
 
 // Create new user
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const result = await userService.registerUser(req?.body);
     res.status(httpStatus.CREATED).json({
       message: "User created successfully",
       data: result,
     });
-  } catch (error: any) {
-    next(error);
   }
-};
+);
 
 // Get all users
-const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const result = await userService.retrieveAllUsers();
     res.json({
       message: "Users retrieved successfully",
       data: result,
     });
-  } catch (error: any) {
-    next(error);
   }
-};
+);
 
 // User controller object
 const userController = {
