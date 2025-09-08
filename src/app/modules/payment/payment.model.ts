818c1db3b0cda06eb5e121566a1188bb7f722184
zahid-official <zahid.official8@gmail.com>
2025-09-08 21:57:49 +0600

    Create mongoose model for payment

---
+++ src/app/modules/payment/payment.model.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+import { IPayment, PaymentStatus } from "./payment.interface";
+
+// Mongoose schema definition for payment
+const paymentSchema = new Schema<IPayment>(
+  {
+    booking: {
+      type: Schema.Types.ObjectId,
+      ref: "Booking",
+      required: true,
+      unique: true,
+    },
+    transactionId: { type: String, required: true, unique: true },
+    amount: { type: Number, required: true },
+    paymentGateway: { type: Schema.Types.Mixed },
+    invoiceUrl: { type: String },
+    status: {
+      type: String,
+      enum: Object.values(PaymentStatus),
+      default: PaymentStatus.UNPAID,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+// Create mongoose model from payment schema
+const Payment = model<IPayment>("Payment", paymentSchema, "paymentCollection");
+export default Payment;
