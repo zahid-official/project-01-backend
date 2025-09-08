380a0086a557b31ddd7370508e859adfa4486c35
zahid-official <zahid.official8@gmail.com>
2025-09-09 03:24:08 +0600

    Define booking routes

--- src/app/modules/booking/booking.routes.ts
+++ src/app/modules/booking/booking.routes.ts
@@ -1,7 +1,60 @@
 import { Router } from "express";
+import { Role } from "../user/user.interface";
+import bookingController from "./booking.controller";
+import validateToken from "../../middlewares/validateToken";
+import validateSchema from "../../middlewares/validateSchema";
+import {
+  createBookingZodSchema,
+  updateBookingZodSchema,
+} from "./booking.validation";
 
 // Initialize router
 const router = Router();
+
+// Get all bookings
+router.get(
+  "/",
+  validateToken(Role.SUPER_ADMIN, Role.SUPER_ADMIN),
+  bookingController.getAllBookings
+);
+
+// Get user bookings
+router.get(
+  "/my-bookings",
+  validateToken(...Object.values(Role)),
+  bookingController.getUserBookings
+);
+
+// Get single booking
+router.get(
+  "/:id",
+  validateToken(...Object.values(Role)),
+  bookingController.getSingleBooking
+);
+
+// Create new booking
+router.post(
+  "/create",
+  validateToken(...Object.values(Role)),
+  validateSchema(createBookingZodSchema),
+  bookingController.createBooking
+);
+
+// Update booking
+router.patch(
+  "/:id",
+  validateToken(...Object.values(Role)),
+  validateSchema(updateBookingZodSchema),
+  bookingController.updateBooking
+);
+
+// Delete booking
+router.delete(
+  "/:id",
+  validateToken(...Object.values(Role)),
+  validateToken(Role.SUPER_ADMIN, Role.SUPER_ADMIN),
+  bookingController.deleteBooking
+);
 
 // Export booking routes
 const bookingRoutes = router;
