b8d6cc306b12f2b01d44124a65c5be37d492313c
zahid-official <zahid.official8@gmail.com>
2025-09-09 02:51:49 +0600

    Zod scheme for booking validation

---
+++ src/app/modules/booking/booking.validation.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+import { Types } from "mongoose";
+import { BookingStatus } from "./booking.interface";
+
+// Zod scheme for new booking creation
+export const createBookingZodSchema = z.object({
+  // Tour (ObjectId)
+  tour: z
+    .string({
+      error: (issue) =>
+        issue.input === undefined
+          ? "Tour is required"
+          : "Tour must be a string objectId",
+    })
+    .refine((value) => Types.ObjectId.isValid(value), {
+      error: "Invalid ObjectId",
+    }),
+
+  // Guests
+  guests: z
+    .number({ error: "Guests must be a number" })
+    .min(1, { error: "Guests must be at least 1" }),
+});
+
+// Zod scheme for booking update
+export const updateBookingZodSchema = z.object({
+  // Status
+  status: z.enum(Object.values(BookingStatus), {
+    error: "Invalid booking status",
+  }),
+});
