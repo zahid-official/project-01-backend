c2b72b60905bab1dad5a523c579e7c58b9a39c6a
zahid-official <zahid.official8@gmail.com>
2025-09-09 03:46:53 +0600

    Make boilerplate for booking service

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,3 +1,39 @@
+// Get all bookings
+const getAllBookings = async () => {
+  return {
+    data: { data: 0 },
+    meta: { meta: 0 },
+  };
+};
+
+// Get user bookings
+const getUserBookings = async () => {
+  return {
+    data: { data: 0 },
+    meta: { meta: 0 },
+  };
+};
+
+// Get single bookings
+const getSingleBooking = async () => {
+  return {};
+};
+
+// Create new booking
+const createBooking = async () => {
+  return {};
+};
+
+// Update booking
+const updateBooking = async () => {
+  return {};
+};
+
+// Delete booking
+const deleteBooking = async () => {
+  return {};
+};
+
 // Booking service object
 const bookingService = {
   getAllBookings,
